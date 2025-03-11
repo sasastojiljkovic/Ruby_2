e15b9b45d3c32e1fad67aa40043e6a0c4cdfb9bc
Sasa Stojiljkovic <sasa.stojiljkovic1992@gmail.com>
2025-03-11 20:43:30 +0200

    With IF condition

--- exo_16.rb
+++ exo_16.rb
@@ -1,13 +1,16 @@
-puts "Hello, welcome to my super pyramide! How many floors you want between 1 and 25?"
+puts "Hello, welcome to my super pyramide! How many floors do you want on your pyramide?"
 print ">"
 
 floors = gets.chomp.to_i
-(1..floors).each do |i|
 
-space = " " * (floors - i)
-steps = "#" * i
+if floors >= 1 && floors <= 25
+  (1..floors).each do |i|
+  space = " " * (floors - i)
+  steps = "#" * i
+  puts space + steps
+  end 
 
-puts space + steps
-
-end
+else
+  puts "Please enter a number between 1 and 25"
+  end
 
